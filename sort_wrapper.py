e791196693b1f3f6b206981739bdff6f4aeb259c
dvrbanic <dominik.vrbanic7@gmail.com>
2021-05-22 20:10:01 +0200

    Added sort.py

---
+++ sort_wrapper.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+
+import rospy
+
+from uav_object_tracking_msgs.msg import object, objectList
+
+# class ..
+# placeholder for wrapper class
+
+
+if __name__ == '__main__':
+    rospy.init_node('sort')
+
+    #class.run()
